0b37f77777c51220877f12fb3cc3500bdaf481bf
Morenike Oyewole <herroyalpianist@gmail.com>
2021-08-22 00:31:24 +0100

    added scroll animations

--- script.js
+++ script.js
@@ -56,3 +56,30 @@ window.onload = function () {
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
+
+
+const faders = document.querySelectorAll(".fade");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
